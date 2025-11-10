a56a0d5cf74b857bd92ef976f5639fac80d35966
Óscar <161454630+oscarlb1@users.noreply.github.com>
2025-11-10 20:07:36 +0100

    ejemplo promesa

--- UD3/15_this/this.js
+++ UD3/15_this/this.js
@@ -1,10 +1,29 @@
-const test = {
-    prop: 42,
-    func: function() {
-        // setTimeout(function() {
-            console.log(this.prop)
-        // }, 1000)
-    },
-  };
+// const test = {
+//     prop: 42,
+//     func: function() {
+//         // setTimeout(function() {
+//             console.log(this.prop)
+//         // }, 1000)
+//     },
+//   };
 
-test.func()
+// test.func()
+
+function crearFichero(nombre, extension) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const exito = Math.random() > 0.5 
+            if (exito) {
+                var mensajeExitoso = {nombreFichero:nombre, extensionFichero:extension}
+                resolve(mensajeExitoso)
+            } else{
+                var error  = 'dio error'
+                reject(error)
+            }
+        }, 2000);
+    })
+}
+
+crearFichero('Oscar', '.txt')
+    .then((res => { console.log(res, 'exito');}))
+    .catch((err => { console.log(err, 'error');}))
