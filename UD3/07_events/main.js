f8f27e5fb8a519a89065de3f31241c967551140f
Óscar <161454630+oscarlb1@users.noreply.github.com>
2025-11-10 16:13:54 +0100

    ud3/07 hecho

--- UD3/07_events/main.js
+++ UD3/07_events/main.js
@@ -1,3 +1,20 @@
-window.onload = function(e) {
-    console.log('documento cargado')
-}
+window.onload = function() {
+    console.log('documento cargado');
+
+    for (let i = 1; i <= 3; i++) {
+        const link = document.getElementById(`enlace_${i}`);
+        const content = document.getElementById(`contenidos_${i}`);
+
+        link.onclick = function(e) {
+            e.preventDefault(); 
+
+            if (content.style.display === 'none') {
+                content.style.display = 'block';
+                link.innerHTML = 'Ocultar contenidos';
+            } else {
+                content.style.display = 'none';
+                link.innerHTML = 'Mostrar contenidos';
+            }
+        };
+    }
+};
